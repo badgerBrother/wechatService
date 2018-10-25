a9993cf7872057d2e43e634734dac15e7c56bc2b
wangluqiong <lqwang@ling-ban.com>
2018-10-25 18:03:18 +0800

    初始化springboot

---
+++ src/main/java/com/badgerBrother/wechatService/WechatServiceApplication.java
@@ -0,0 +1,12 @@
+package com.badgerBrother.wechatService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatServiceApplication.class, args);
+	}
+}
